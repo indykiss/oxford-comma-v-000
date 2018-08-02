39318bbf3fa939530dfc67af03730c3ace70df59
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-02 00:59:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,12 @@
 def oxford_comma(array)
 
-if array.length == 2
+if array.length < 2
   array.join(", ")
   
   elsif array.length > 2 
     array[-1].prepend "and "
 	  array.join(", ")
-	    
+	  
+
   end 	      
 end
